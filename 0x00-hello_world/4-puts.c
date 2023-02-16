b5911b31d0295f2b4111c656989a8957408766c6
Adiba11 <yido.ibtj@gmail.com>
2023-02-16 12:58:06 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,16 @@
 #!/bin/bash
-#include <stdio.h>
+#include <studio.h>
+
+
+
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ * main - prints "programing is like building a multilingual 
+ * puzzle, followed by a newline."
+ * return 0.
  */
 int main(void)
+
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+     
+      puts("\"programing is like building a multilingualmpuzzle")
+      return(0);	      
